697fe64e775e7ecb0a2b46d26e2b78344d62e818
Michael <micbarash@gmail.com>
2018-02-24 14:11:58 +0200

    fixed timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
-  #write your code here
+  attr_reader :seconds, :time_string, :initialize
+
+  def initialize
+  	@hours = 0
+  	@minutes = 0
+  	@seconds = 0
+  end
+
+  def time_string
+  	return @hours.to_s + ":" + @minutes.to_s + ":" + @seconds.to_s
+  end
+
+  def seconds=(time)
+  	if time == 0
+  		@seconds = '00'
+  		@minutes = '00'
+  		@hours = '00'
+  	elsif time < 60
+  		@minutes = '00'
+  		@hours = '00'
+  		@seconds = time.to_s
+  	elsif time > 60 && time < 3600
+  		@hours = '00'
+  		@minutes = '0' + (time / 60).to_s
+  		@seconds = '0' + (time % 60).to_s
+  	elsif time > 3600
+  		@hours = '0' + (time / 3600).to_s
+  		@minutes = '0' + ((time % 3600) / 60).to_s 
+  		@seconds = ((time % 3600) % 60).to_s
+  	end
+  end
 end
+
+@timer = Timer.new
+@timer.seconds = 4000
+puts @timer.time_string
